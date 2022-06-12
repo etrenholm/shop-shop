59f9fa59dce79c0d92dfbe63f69a0c0574a24481
Erica <erica.trenholm48@gmail.com>
2022-06-12 17:04:27 -0400

    implement persistance

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
-
-import { useStoreContext } from '../../utils/GlobalState';
-import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions'
+import { useStoreContext } from "../../utils/GlobalState";
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
+  const [state, dispatch] = useStoreContext();
+
   const {
     image,
     name,
@@ -13,27 +15,27 @@ function ProductItem(item) {
     price,
     quantity
   } = item;
-  
-  const [state, dispatch] = useStoreContext();
 
-  const { cart } = state;
+  const { cart } = state
 
   const addToCart = () => {
-    // find the cart item with id
     const itemInCart = cart.find((cartItem) => cartItem._id === _id)
-
-    // if there is a match, call UPDATE with a new purchase quantity
     if (itemInCart) {
       dispatch({
         type: UPDATE_CART_QUANTITY,
         _id: _id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
-      })
+      });
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
     } else {
       dispatch({
         type: ADD_TO_CART,
         product: { ...item, purchaseQuantity: 1 }
-      })
+      });
+      idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
     }
   }
 
--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -4,3 +4,67 @@ export function pluralize(name, count) {
   }
   return name + 's'
 }
+
+export function idbPromise(storeName, method, object) {
+
+  return new Promise((resolve, reject) => {
+
+    // open connection to the database shop-shop with the ersion of 1
+    const request = window.indexedDB.open('shop-shop', 1)
+
+    // create variables to hold reference to the database, transaction, and bject store
+    let db, tx, store;
+
+    // if version has changed or if first time using database, run this method and create the three object stores
+    request.onupgradeneeded = function(e) {
+      const db = request.result// create object store for each type of data and set primary key index
+      db.createObjectStore('products', { keyPath: '_id' })
+      db.createObjectStore('categories', { keyPath: '_id' })
+      db.createObjectStore('cart', { keyPath: '_id' })
+    }
+
+    // handle any errors
+    request.onerror = function(e) {
+      console.log('There was any error.')
+    }
+
+    // on database open success
+    request.onsuccess = function(e) {
+      // save a reference of the database to the db variable
+      db = request.result
+      // open a transaction to do whatever we pass into storeName
+      tx = db.transaction(storeName, 'readwrite')
+      // save a reference to that object store
+      store = tx.objectStore(storeName)
+
+      // handle any errors
+      db.onerror = function(e) {
+        console.log('error', e)
+      }
+
+      switch (method) {
+        case 'put':
+          store.put(object)
+          resolve(object)
+          break;
+        case 'get':
+          const all = store.getAll()
+          all.onsuccess = function() {
+            resolve(all.result)
+          }
+          break;
+        case 'delete':
+          store.delete(object._id)
+          break;
+        default:
+          console.log('No valid method')
+          break;
+      }
+
+      // when the transaction is complete, close the connection
+      tx.oncomplete = function() {
+        db.close()
+      }
+    }
+  })
+}
